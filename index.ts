e825f8fbb6aef87977a434388581910cdc919de8
Herberts da Silva Cruz Fortuna <herbertscruz@gmail.com>
2022-11-25 15:13:30 -0300

    feat: version 1.1.1

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import authorizationByPermission from './src/middlewares/authorizationByPermission';
+import authorizationByRoles from './src/middlewares/authorizationByRoles';
 import JWTTokenService from './src/services/JWTTokenService';
 import KeycloakAuthorizationService from './src/services/KeycloakAuthorizationService';
 
 export { JWTTokenService };
 export { KeycloakAuthorizationService };
 export { authorizationByPermission };
+export { authorizationByRoles };
